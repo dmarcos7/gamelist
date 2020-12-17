1672d3b06d98a701d1943e419dcfca765cbbd240
dmarcos7 <davidmarcosnatividad@gmail.com>
2020-12-17 13:39:07 +0100

    Inicio de proyecto

---
+++ prueba/src/packPrueba/Main.java
@@ -0,0 +1,20 @@
+package packPrueba;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mostrarElementos();
+	}
+	
+	public static void mostrarElementos(){
+		for (int i=1; i<=10;i++){
+			if(i % 2 == 0){
+				System.out.println(i + " Es un elemento par");
+			}else {
+				System.out.println(i+" Es un elemento impar");
+			}
+		}
+	}
+
+}
